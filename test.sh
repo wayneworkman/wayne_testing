9cb6b5b062cccf69fd621ecc103ed39e39733372
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2023-05-10 21:38:59 -0500

    scripting

--- test.sh
+++ test.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
-for count in {0..20}; do
-    echo ${count}
+for count in {0..3}; do
+    git checkout four
+    git pull
+    git checkout -b ${count}
+    echo "${count}" >> log.txt
+    git commit -am "${count}"
+    git push
+
+    git checkout three
+    git merge four
+    git push
+
+    git checkout two
+    git merge three
+    git push
+
+    git checkout one
+    git merge two
+    git push
+    
 done
